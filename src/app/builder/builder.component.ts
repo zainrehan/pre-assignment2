0b8cd9df7194c78883e1816c06f94ad354cc61f2
zainrehan <zainrehan9150@gmail.com>
2023-04-27 19:18:16 +0500

    aa

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -11,9 +11,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./builder.component.css'],
 })
 export class BuilderComponent implements OnInit {
-
-  index: number=0;
-  answerText: string[]= []
+  index: number = 0;
+  answerText: string[] = [];
   viewQuestions: any[] = [];
   answerOption = {
     Answer: null,
@@ -29,48 +28,46 @@ export class BuilderComponent implements OnInit {
     private dservice: DataService,
     private router: Router,
     private Mat: MatDialog,
-    private toastr :ToastrService
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
     this.viewQuestions = this.dservice.getGlobalQuestions();
-    this.toastr.success('Welcome to Builder Component', 'from/builder',{
-      timeOut:2000,
-      progressBar:true,
-      progressAnimation:'increasing',
+    this.toastr.success('Welcome to Builder Component', 'from/builder', {
+      timeOut: 2000,
+      progressBar: true,
+      progressAnimation: 'increasing',
     });
   }
 
-  abc(data:NgForm){
-    for(let i=0; i<this.dservice.getGlobalQuestions().length;i++){
-
-    if(this.viewQuestions[this.index].type === 'textarea'){
-      this.dservice.globalAnswers({
-        type: 'textarea',
-        Answer: data.value[`taAnswer${this.index}`],
-        Option1: null,
-        Option2: null,
-        Option3: null,
-        Option4: null,
-        Option5: null,
-      });
-  }
-  if(this.viewQuestions[this.index].type === 'checkbox')
-  {
+  abc(data: NgForm) {
+    for (let i = 0; i < this.dservice.getGlobalQuestions().length; i++) {
+      if (this.viewQuestions[this.index].type === 'textarea') {
         this.dservice.globalAnswers({
-      type: 'checkbox',
-      Answer: null,
-      Option1: data.value[`opt1${this.index}`],
-      Option2: data.value[`opt2${this.index}`],
-      Option3: data.value[`opt3${this.index}`],
-      Option4: data.value[`opt4${this.index}`],
-      Option5: null,
-    });
-  }
-  this.index++
-}
-this.router.navigate(['form/answers']);
-this.Mat.closeAll();
+          type: 'textarea',
+          Answer: data.value[`taAnswer${this.index}`],
+          Option1: null,
+          Option2: null,
+          Option3: null,
+          Option4: null,
+          Option5: null,
+        });
+      }
+      if (this.viewQuestions[this.index].type === 'checkbox') {
+        this.dservice.globalAnswers({
+          type: 'checkbox',
+          Answer: null,
+          Option1: data.value[`opt1${this.index}`],
+          Option2: data.value[`opt2${this.index}`],
+          Option3: data.value[`opt3${this.index}`],
+          Option4: data.value[`opt4${this.index}`],
+          Option5: data.value[`catext${this.index}`],
+        });
+      }
+      this.index++;
+    }
+    this.router.navigate(['form/answers']);
+    this.Mat.closeAll();
   }
 
   answerPage() {
@@ -78,39 +75,37 @@ this.Mat.closeAll();
     this.Mat.closeAll();
   }
 
-  onSubmit(form:NgForm){
-console.log(form)
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 
-//   onSubmitTextAnswer() {
-//     this.dservice.globalAnswers({
-//       type: 'textarea',
-//       Answer: this.answerText[this.index],
-//       Option1: null,
-//       Option2: null,
-//       Option3: null,
-//       Option4: null,
-//       Option5: null,
-//     });
-//     this.index++;
+  //   onSubmitTextAnswer() {
+  //     this.dservice.globalAnswers({
+  //       type: 'textarea',
+  //       Answer: this.answerText[this.index],
+  //       Option1: null,
+  //       Option2: null,
+  //       Option3: null,
+  //       Option4: null,
+  //       Option5: null,
+  //     });
+  //     this.index++;
 
-//     console.log(this.dservice.getglobalAnswers());
-//   }
+  //     console.log(this.dservice.getglobalAnswers());
+  //   }
 
-//   onSubmitcheckAnswer() {
-//     this.dservice.globalAnswers({
-//       type: 'checkbox',
-//       Answer: null,
-//       Option1: this.answerOption.Option1,
-//       Option2: this.answerOption.Option2,
-//       Option3: this.answerOption.Option3,
-//       Option4: this.answerOption.Option4,
-//       Option5: this.answerOption.Option5,
-//     });
+  //   onSubmitcheckAnswer() {
+  //     this.dservice.globalAnswers({
+  //       type: 'checkbox',
+  //       Answer: null,
+  //       Option1: this.answerOption.Option1,
+  //       Option2: this.answerOption.Option2,
+  //       Option3: this.answerOption.Option3,
+  //       Option4: this.answerOption.Option4,
+  //       Option5: this.answerOption.Option5,
+  //     });
 
-//     console.log(this.dservice.getglobalAnswers());
-//     console.log(this.dservice.getGlobalQuestions());
-//   }
-
-
+  //     console.log(this.dservice.getglobalAnswers());
+  //     console.log(this.dservice.getGlobalQuestions());
+  //   }
 }
