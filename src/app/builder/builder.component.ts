57c3744492d8d9fef24e6f51c1d8e054f8ba8b6e
zainrehan <zainrehan9150@gmail.com>
2023-04-27 18:29:11 +0500

    update

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -41,25 +41,37 @@ export class BuilderComponent implements OnInit {
     });
   }
 
-  // abc(formdata:NgForm){
-  //   console.log(formdata)
-  //   while(this.index===this.dservice.getGlobalQuestions().length){
-  //     this.answerOption= {
-  //       Answer: formdata.form.value['taAnswer0'],
-  //       Option1: false,
-  //       Option2: false,
-  //       Option3: false,
-  //       Option4: false,
-  //       Option5: null,
-  //     };
-  //     this.index++ 
-  //   }  
-  //   console.log(this.dservice.getGlobalQuestions().length)
-  //   console.log(this.answerOption)  
-  //   // this.index++
-  //   // console.log(this.answerOption)  
-  // }
+  abc(data:NgForm){
+    for(let i=0; i<this.dservice.getGlobalQuestions().length;i++){
 
+    if(this.viewQuestions[this.index].type === 'textarea'){
+      this.dservice.globalAnswers({
+        type: 'textarea',
+        Answer: data.value[`taAnswer${this.index}`],
+        Option1: null,
+        Option2: null,
+        Option3: null,
+        Option4: null,
+        Option5: null,
+      });
+  }
+  if(this.viewQuestions[this.index].type === 'checkbox')
+  {
+        this.dservice.globalAnswers({
+      type: 'checkbox',
+      Answer: null,
+      Option1: data.value[`opt1${this.index}`],
+      Option2: data.value[`opt2${this.index}`],
+      Option3: data.value[`opt3${this.index}`],
+      Option4: data.value[`opt4${this.index}`],
+      Option5: null,
+    });
+  }
+  this.index++
+}
+this.router.navigate(['form/answers']);
+this.Mat.closeAll();
+  }
 
   answerPage() {
     this.router.navigate(['form/answers']);
@@ -70,33 +82,35 @@ export class BuilderComponent implements OnInit {
 console.log(form)
   }
 
-  onSubmitTextAnswer() {
-    this.dservice.globalAnswers({
-      type: 'textarea',
-      Answer: this.answerText[this.index],
-      Option1: null,
-      Option2: null,
-      Option3: null,
-      Option4: null,
-      Option5: null,
-    });
-    this.index++;
+//   onSubmitTextAnswer() {
+//     this.dservice.globalAnswers({
+//       type: 'textarea',
+//       Answer: this.answerText[this.index],
+//       Option1: null,
+//       Option2: null,
+//       Option3: null,
+//       Option4: null,
+//       Option5: null,
+//     });
+//     this.index++;
 
-    console.log(this.dservice.getglobalAnswers());
-  }
+//     console.log(this.dservice.getglobalAnswers());
+//   }
 
-  onSubmitcheckAnswer() {
-    this.dservice.globalAnswers({
-      type: 'checkbox',
-      Answer: null,
-      Option1: this.answerOption.Option1,
-      Option2: this.answerOption.Option2,
-      Option3: this.answerOption.Option3,
-      Option4: this.answerOption.Option4,
-      Option5: this.answerOption.Option5,
-    });
+//   onSubmitcheckAnswer() {
+//     this.dservice.globalAnswers({
+//       type: 'checkbox',
+//       Answer: null,
+//       Option1: this.answerOption.Option1,
+//       Option2: this.answerOption.Option2,
+//       Option3: this.answerOption.Option3,
+//       Option4: this.answerOption.Option4,
+//       Option5: this.answerOption.Option5,
+//     });
 
-    console.log(this.dservice.getglobalAnswers());
-    console.log(this.dservice.getGlobalQuestions());
-  }
+//     console.log(this.dservice.getglobalAnswers());
+//     console.log(this.dservice.getGlobalQuestions());
+//   }
+
+
 }
